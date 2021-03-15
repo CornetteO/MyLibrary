87f2da3942c577d4b1a9f838739ddd3b35a95524
Olivier Cornette <cornette.olivier@gmail.com>
2021-03-15 20:14:49 +0100

    Ajout de la classe main

---
+++ src/MyApplicationMain.java
@@ -0,0 +1,14 @@
+
+public class MyApplicationMain {
+
+	public static void main(String[] args) {
+		//Person : name - MaxBooks - registrationDate
+		String[] membres = new String[50];
+		String[] livres = {};
+		int[] emprunts = {};
+		
+		System.out.println("Bonjour, bienvenue dans ce programme");
+
+	}
+
+}
